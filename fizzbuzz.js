772b53765ce71c11c084782a7a69c53e9e84a985
mvfb <marina.braverman@gmail.com>
2015-06-17 20:59:33 -0700

    Chapter 2 exercises from Eloquent JS

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,5 @@
+// Eloquent JS, Chapter 2, exercise 2
+
 var printThis = "";
 for (i = 1; i <= 100; i++) {
   if (i % 3 == 0) printThis += "Fizz";
---
+++ triangle.js
@@ -0,0 +1,7 @@
+// Eloquent JS, Chapter 2, exercise 1
+
+var printThis = "#";
+for (i = 0; i < 7; i++) {
+  console.log(printThis);
+  printThis += "#";
+}
