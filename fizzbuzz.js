18fe9ba6cc58209350eef213ef7ebb45fbde5bef
mvfb <marina.braverman@gmail.com>
2015-06-17 20:50:47 -0700

    FizzBuzz from Eloquent JS

---
+++ fizzbuzz.js
@@ -0,0 +1,7 @@
+var printThis = "";
+for (i = 1; i <= 100; i++) {
+  if (i % 3 == 0) printThis += "Fizz";
+  if (i % 5 == 0) printThis += "Buzz";
+  console.log(printThis || i);
+  printThis = "";
+}
